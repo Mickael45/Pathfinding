b51336af707ac9eb72c13a5da248bbf02cf15bf9
Mickael Gomes <mickaelgomesconsulting@gmail.com>
2020-12-18 13:53:10 +0100

    Improved wall creation experience

--- src/NodeMap.tsx
+++ src/NodeMap.tsx
@@ -1,4 +1,3 @@
-import type { MouseEvent } from "react";
 import "./NodeMap.css";
 import { getConfig } from "./config";
 
@@ -12,17 +11,22 @@ const {
   NUMBER_OF_ROWS,
 } = getConfig();
 
-const setNodeAsStart = (node: MouseEvent<DataSetElement>) => {
+const setNodeAsEmpty = (node: React.MouseEvent) => {
+  (node.target as DataSetElement).id = "";
+  (node.target as DataSetElement).style.backgroundColor = "";
+};
+
+const setNodeAsStart = (node: React.MouseEvent) => {
   (node.target as DataSetElement).id = START_NODE_ID;
   (node.target as DataSetElement).style.backgroundColor = START_NODE_COLOR;
 };
 
-const setNodeAsEnd = (node: MouseEvent<DataSetElement>) => {
+const setNodeAsEnd = (node: React.MouseEvent) => {
   (node.target as DataSetElement).id = END_NODE_ID;
   (node.target as DataSetElement).style.backgroundColor = END_NODE_COLOR;
 };
 
-const setNodeAsWall = (node: MouseEvent<DataSetElement>) => {
+const setNodeAsWall = (node: MouseEvent | React.MouseEvent) => {
   (node.target as DataSetElement).style.backgroundColor = WALL_COLOR;
 };
 
@@ -30,18 +34,47 @@ const doesStartNodeExist = () => document.getElementById(START_NODE_ID);
 
 const doesEndNodeExist = () => document.getElementById(END_NODE_ID);
 
-const isNodeEmpty = (node: DataSetElement) => (node.style.backgroundColor = "white");
+const isNodeEmpty = (node: React.MouseEvent) => (node.target as DataSetElement).style.backgroundColor === "";
 
-const handleNodeClick = (e: MouseEvent<DataSetElement>) => {
-  if (!isNodeEmpty(e.target as DataSetElement)) {
-    return;
-  }
+const setEventListeners = () => {
+  const DOWN_STATE = "down";
+  const UP_STATE = "up";
+  let mouseState = UP_STATE;
+
+  const isMouseDown = () => mouseState === DOWN_STATE;
+
+  const onMouseDown = () => (mouseState = DOWN_STATE);
+  const onMouseUp = () => (mouseState = UP_STATE);
+
+  const onMouseOver = (e: MouseEvent) => {
+    if (isMouseDown() && (e.target as DataSetElement).dataset.index) {
+      setNodeAsWall(e);
+    }
+  };
+
+  const addEventListeners = () => {
+    window.addEventListener("mousedown", onMouseDown);
+    window.addEventListener("mouseup", onMouseUp);
+    window.addEventListener("mouseover", onMouseOver);
+  };
+
+  addEventListeners();
+
+  return {
+    removeEventListeners: () => {
+      window.removeEventListener("mousedown", onMouseDown);
+      window.removeEventListener("mouseup", onMouseUp);
+    },
+  };
+};
+
+const handleNodeClick = (e: React.MouseEvent) => {
   if (!doesStartNodeExist()) {
     return setNodeAsStart(e);
   } else if (!doesEndNodeExist()) {
     return setNodeAsEnd(e);
   }
-  return setNodeAsWall(e);
+  return isNodeEmpty(e) ? setNodeAsWall(e) : setNodeAsEmpty(e);
 };
 
 const createNode = (index: number) => <div className="square" onClick={handleNodeClick} data-index={index} />;
@@ -49,6 +82,7 @@ const createNode = (index: number) => <div className="square" onClick={handleNod
 const renderNodes = () => {
   const nodeMap = [];
   const nodesAmount = NUMBER_OF_COLUMNS * NUMBER_OF_ROWS;
+  setEventListeners();
 
   for (let i = 0; i < nodesAmount; i++) {
     nodeMap.push(createNode(i));
